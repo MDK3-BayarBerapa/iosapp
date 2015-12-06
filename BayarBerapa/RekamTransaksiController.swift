4d582775ae99f5657a1019fc919103f155d23f99
berkedel <akhmad.hadi@gmail.com>
2015-12-06 15:08:39 +0900

    Add login/logout page and change menu

---
+++ BayarBerapa/RekamTransaksiController.swift
@@ -0,0 +1,9 @@
+//
+//  RekamTransaksiController.swift
+//  BayarBerapa
+//
+//  Created by hadi on 12/6/15.
+//  Copyright © 2015 Akhmad Syaikhul Hadi. All rights reserved.
+//
+
+import Foundation
